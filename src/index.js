ccaa88abae166e6e4bbf4ec85fb27c68b05675c4
Eldon Victor Ratzlaff Koop <mecanicark@gmail.com>
2023-01-28 18:05:22 -0300

    Add remove delete and add tasks

--- src/index.js
+++ src/index.js
@@ -1,40 +1,79 @@
 import './style.css';
 
-const tasks = [
-  {
-    description: 'Take a Shower',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Cook today',
-    completed: false,
-    index: 10,
-  },
-  {
-    description: 'Buy groceries',
-    completed: false,
-    index: 3,
-  },
-  {
-    description: ' Homework',
-    completed: false,
-    index: 8,
-  },
-  {
-    description: ' List Project',
-    completed: false,
-    index: 4,
-  },
-];
+/*
+ localStorage.setItem('tasks', JSON.stringify(tasks));
+*/
+const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+// Add an element
+const addTask = document.getElementById('addItem');
+addTask.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    window.location.reload();
+    let maxId = 0;
+    for (let i = 0; tasks.length > i; i += 1) {
+      if (maxId < tasks[i].index) {
+        maxId = tasks[i].index;
+      }
+    }
+
+    tasks.push({
+      description: addTask.value,
+      Completed: false,
+      index: maxId + 1,
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    window.location.reload();
+  }
+});
 
 const show = () => {
   const getList = document.getElementById('todo-list');
   for (let i = 0; i < tasks.length; i += 1) {
     const item = document.createElement('li');
-    item.innerHTML = `<input type="checkbox" class="check" />  ${tasks[i].description}`;
+    let chek = '';
+    if (tasks[i].Completed === true) {
+      chek = 'checked';
+    }
+    item.innerHTML = `<input  type="checkbox" ${chek} class="check" onchange=checkboxx(${i}) />  ${tasks[i].description}
+    <button onclick=deletebtn(${tasks[i].index}) class="deletebtn">Delete</button>`;
     getList.appendChild(item);
   }
 };
 
 show();
+
+// remove an element
+window.deletebtn = (index) => {
+  const result = tasks.filter((item) => item.index !== index);
+  const newtasks = result;
+  localStorage.setItem('tasks', JSON.stringify(newtasks));
+  window.location.reload();
+};
+
+// checket
+window.checkboxx = (i) => {
+  if (tasks[i].Completed === false) {
+    tasks[i].Completed = true;
+  } else { tasks[i].Completed = false; }
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+// remove cheked
+const removeTask = document.getElementById('clear-button');
+removeTask.addEventListener('click', () => {
+  let tasksLength = tasks.length;
+
+  for (let i = 0; tasksLength > i; i += 1) {
+    if (tasks[i].Completed === true) {
+      tasks.splice(i, 1);
+      const resultcheck = tasks;
+      const newtaskscheck = resultcheck;
+      localStorage.setItem('tasks', JSON.stringify(newtaskscheck));
+      i -= 1;
+      tasksLength -= 1;
+    }
+  } window.location.reload();
+});
